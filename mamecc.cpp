60234baaad3c243f8019b9c2adcff8cde8148092
irimame <irimamex@gmail.com>
2025-08-15 16:59:29 +0900

    Add add and sub operation

--- mamecc.cpp
+++ mamecc.cpp
@@ -2,15 +2,36 @@
 #include <cstdlib>
 
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        std::cerr << "The number of arguments must be 2." << std::endl;
-        return 1;
+  if (argc != 2) {
+    std::cerr << "Error: The number of arguments must be 2" << std::endl;
+    return 1;
+  }
+
+  char *p = argv[1];
+
+  std::cout << ".intel_syntax noprefix" << std::endl;
+  std::cout << ".globl main" << std::endl;
+  std::cout << "main: " << std::endl;
+  std::cout << "  mov rax, " << std::strtol(p, &p, 10) << std::endl;
+
+  while (*p) {
+
+    if (*p == '+') {
+      ++p;
+      std::cout << "  add rax, " << std::strtol(p, &p, 10) << std::endl;
+      continue;
     }
 
-    std::cout << ".intel_syntax noprefix" << std::endl;
-    std::cout << ".globl main" << std::endl;
-    std::cout << "main: " << std::endl;
-    std::cout << "  mov rax, " << argv[1] << std::endl;
-    std::cout << "  ret" << std::endl;
-    return 0;
+    if (*p == '-') {
+      ++p;
+      std::cout << "  sub rax, " << std::strtol(p, &p, 10) << std::endl;
+      continue;
+    }
+
+    std::cerr << "Error: Unexpected character: '" << *p << "'" << std::endl;
+    return 1;
+  }
+
+  std::cout << "  ret" << std::endl;
+  return 0;
 }
